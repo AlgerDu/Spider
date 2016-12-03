a415650618de234f99cac56cd40928761239df65
Alger <AlgerDu@outlook.com>
2016-12-03 18:40:23 +0800

    添加 Util 测试项目

---
+++ D.Util.Test/TestModel.cs
@@ -0,0 +1,25 @@
+﻿using D.Util.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace D.Util.Test
+{
+    public class TestEvent : IEvent
+    {
+        public Guid GUID { get; set; }
+
+        public DateTime TimeStamp { get; set; }
+    }
+
+    public class TestHandler : IEventHandler<TestEvent>
+    {
+        public void Handle(TestEvent e)
+        {
+            Thread.Sleep(1000);
+        }
+    }
+}
