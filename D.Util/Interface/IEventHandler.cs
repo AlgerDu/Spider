12e044080754677a7b21fa067e8d1b93ee18d97b
Alger <AlgerDu@outlook.com>
2016-12-03 18:40:03 +0800

    添加 Util 项目

---
+++ D.Util/Interface/IEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Util.Interface
+{
+    /// <summary>
+    /// 事件处理程序
+    /// </summary>
+    public interface IEventHandler<Event>
+        where Event : IEvent
+    {
+        /// <summary>
+        /// 处理事件
+        /// </summary>
+        /// <param name="e"></param>
+        void Handle(Event e);
+    }
+}
