3001f9e38cf1f5717f83299427c004754e4dcf29
Alger <AlgerDu@outlook.com>
2016-12-17 19:57:55 +0800

    实现 AddUrl 重载 添加url定时检测函数

--- D.Spider.Core/UrlManager.cs
+++ D.Spider.Core/UrlManager.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using D.Spider.Core.Events;
 using D.Util.Interface;
+using System.Timers;
 
 namespace D.Spider.Core
 {
@@ -21,10 +22,18 @@ namespace D.Spider.Core
         List<IUrl> _waitingCrawlUrl;
         List<IUrl> _crawlingUrl;
 
+        Timer _urlCheckTimer;//定时检测是否有 Url 需要检测
+        const int _urlCheckInterval = 10000;//定时检测的时间间隔
+
         public UrlManager(IEventBus eventBus, ILoggerFactory loggerFactory)
         {
             _eventBus = eventBus;
             _logger = loggerFactory.CreateLogger<UrlManager>();
+
+            _urlCheckTimer = new Timer(_urlCheckInterval);
+            _urlCheckTimer.Elapsed += new ElapsedEventHandler(UrlCheckTimer_Elapsed);
+
+            _urlCheckTimer.Start();
 
             _allUrl = new List<IUrl>();
             _waitingCrawlUrl = new List<IUrl>();
@@ -35,6 +44,8 @@ namespace D.Spider.Core
 
         ~UrlManager()
         {
+            _urlCheckTimer.Stop();
+
             _allUrl.Clear();
             _waitingCrawlUrl.Clear();
             _crawlingUrl.Clear();
@@ -42,7 +53,7 @@ namespace D.Spider.Core
 
         #region IUrlManager 接口实现
 
-        public void AddUrl(IUrl url)
+        public IUrl AddUrl(IUrl url)
         {
             var find = _allUrl.Find(uu => uu.Equal(url));
 
@@ -54,26 +65,50 @@ namespace D.Spider.Core
 
                 find = url;
             }
+            else
+            {
+                find.Interval = url.Interval;
+            }
 
-            if (url.NeedCrawl() && _waitingCrawlUrl.Find(uu => uu == find) == null)
+            if (url.NeedCrawl() && !IsWaitingCrawl(url))
             {
                 _waitingCrawlUrl.Add(find);
 
                 _eventBus.Publish(new UrlWaitingEvent());
             }
+
+            return find;
+        }
+
+        public IUrl AddUrl(string url)
+        {
+            if (Url.IsUrl(url))
+            {
+                return AddUrl(new Url(url));
+            }
+            else
+            {
+                _logger.LogError(url + " 不是正确的 url");
+                return null;
+            }
+        }
+
+        public IUrl AddUrl(string host, string relativePath)
+        {
+            if (Url.IsUrl(host))
+            {
+                return AddUrl(new Url(host, relativePath));
+            }
+            else
+            {
+                _logger.LogError(host + " 不是正确的 url 主机地址");
+                return null;
+            }
         }
 
         public bool IsWaitingCrawl(IUrl url)
         {
-            foreach (var lUrl in _allUrl)
-            {
-                if (lUrl.Equal(url))
-                {
-                    return true;
-                }
-            }
-
-            return false;
+            return _waitingCrawlUrl.Find(uu => uu.Equal(url)) != null;
         }
 
         public IUrl NextCrawl()
@@ -106,5 +141,35 @@ namespace D.Spider.Core
         }
 
         #endregion
+
+        /// <summary>
+        /// url 定时检测函数
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void UrlCheckTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            _logger.LogInformation("定时检测是否有 Url 需要爬取");
+
+            var urlNeedCrawlCount = 0;
+
+            foreach (var url in _allUrl)
+            {
+                if (url.NeedCrawl() && !IsWaitingCrawl(url))
+                {
+                    _waitingCrawlUrl.Add(url);
+
+                    urlNeedCrawlCount++;
+                }
+            }
+
+            _logger.LogInformation("需要爬取的 Url 数量 " + urlNeedCrawlCount);
+
+            if (urlNeedCrawlCount > 0)
+            {
+                _logger.LogDebug("发布事件 UrlWaitingEvent");
+                _eventBus.Publish(new UrlWaitingEvent());
+            }
+        }
     }
 }
