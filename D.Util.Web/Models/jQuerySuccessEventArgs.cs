084144fcd7382810499a8619d46f490dcec5c152
Alger <AlgerDu@outlook.com>
2016-12-30 20:39:17 +0800

    添加 D.Util.Web 项目 实现 IjQuery 接口

---
+++ D.Util.Web/Models/jQuerySuccessEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Util.Models
+{
+    public class jQuerySuccessEventArgs<T> : EventArgs
+        where T : class
+    {
+        /// <summary>
+        /// 请求成功返回的数据
+        /// </summary>
+        public T Data { get; private set; }
+
+        public jQuerySuccessEventArgs(T data)
+        {
+            Data = data;
+        }
+    }
+}
---
+++ D.Util.Web/Web/jQuery.cs
@@ -0,0 +1,84 @@
+﻿using D.Util.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D.Util.Models;
+using System.Net;
+using System.IO;
+using Newtonsoft.Json;
+
+namespace D.Util.Web
+{
+    /// <summary>
+    /// 使用 HttpWebRequest 和 HttpWebResponse 实现的 jQuer ajax 请求
+    /// </summary>
+    public class jQuery : IjQuery
+    {
+        const int _defaultTimeout = 10000;
+
+        public Task Ajax<T>(AjaxRequestTypes type, string url, object data, EventHandler<jQuerySuccessEventArgs<T>> success, EventHandler<jQueryErrorEventArgs> error = null, int timeout = -1) where T : class
+        {
+            return Task.Run(() =>
+            {
+                HttpWebRequest request = WebRequest.Create(url) as HttpWebRequest;
+                request.Method = type.ToString();
+                request.Timeout = timeout == -1 ? _defaultTimeout : timeout;
+
+                if (type == AjaxRequestTypes.POST && data != null)
+                {
+                    var json = JsonConvert.SerializeObject(data);
+
+                    request.ContentType = "application/json";
+
+                    using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                    {
+                        streamWriter.Write(json);
+                        streamWriter.Flush();
+                    }
+                }
+
+                using (var response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode == HttpStatusCode.OK)
+                    {
+                        Stream respStream = response.GetResponseStream();
+                        using (StreamReader reader = new StreamReader(respStream, Encoding.GetEncoding(response.CharacterSet)))
+                        {
+                            var txt = reader.ReadToEnd();
+                            var rData = TxtToObject<T>(txt);
+                            success?.BeginInvoke(this, new jQuerySuccessEventArgs<T>(rData as T), null, null);
+                        }
+                    }
+                    else
+                    {
+                        error?.BeginInvoke(this, new jQueryErrorEventArgs(), null, null);
+                    }
+                }
+            });
+        }
+
+        public Task Get<T>(string url, EventHandler<jQuerySuccessEventArgs<T>> success) where T : class
+        {
+            return Ajax(AjaxRequestTypes.GET, url, null, success);
+        }
+
+        public Task Post<T>(string url, object data, EventHandler<jQuerySuccessEventArgs<T>> success) where T : class
+        {
+            return Ajax(AjaxRequestTypes.POST, url, data, success);
+        }
+
+        private object TxtToObject<T>(string txt) where T : class
+        {
+            if (typeof(T) == typeof(string))
+            {
+                return txt;
+            }
+            else
+            {
+                return JsonConvert.DeserializeObject<T>(txt);
+            }
+        }
+    }
+}
