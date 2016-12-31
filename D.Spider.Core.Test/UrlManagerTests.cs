552d7ada9e040cc1cda2d76f7c7f5abd56e9fcde
Alger <AlgerDu@outlook.com>
2016-12-31 08:54:47 +0800

    D.Spider.Core 测试程序

---
+++ D.Spider.Core.Test/UrlManagerTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using D.Spider.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Spider.Core.Tests
+{
+    [TestClass()]
+    public class UrlManagerTests
+    {
+        [TestMethod()]
+        public void AddUrlTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
