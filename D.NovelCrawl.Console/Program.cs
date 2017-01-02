bb3422c740f543ddea2380d99800c42935ec875a
Alger <AlgerDu@outlook.com>
2017-01-02 21:14:23 +0800

    小说爬取控制台

---
+++ D.NovelCrawl.Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using D.NovelCrawl.Core;
+using D.Spider.Core;
+using D.Util.Logger.Console;
+using D.Util.Web;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.NovelCrawl.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var spider = new DSpider()
+                .UnityConfigerPath(AppDomain.CurrentDomain.BaseDirectory + @"Unity.config")
+                .Initialization()
+                .Run();
+
+            System.Console.ReadKey();
+        }
+    }
+}
