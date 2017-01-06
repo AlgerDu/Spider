98042ebaca9cb0ddbefb5c2a9c62498ebf97a115
Alger <AlgerDu@outlook.com>
2017-01-06 22:32:29 +0800

    添加接口，用来执行自定义语言 spiderscript 解析 html

---
+++ D.Util.Web/Web/SpiderscriptEngine.cs
@@ -0,0 +1,21 @@
+﻿using D.Util.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace D.Util.Web
+{
+    /// <summary>
+    /// ISpiderscriptEngine 实现
+    /// </summary>
+    public class SpiderscriptEngine : ISpiderscriptEngine
+    {
+        public JToken Run(string doc, string spiderscriptCode)
+        {
+            return new JObject();
+        }
+    }
+}
