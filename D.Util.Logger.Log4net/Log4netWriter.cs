d2d0057c7827e7dd68f3bb7cf7c67c53e3bfa967
Alger <AlgerDu@outlook.com>
2017-02-03 15:43:58 +0800

    添加封装 log4net 的封装类

---
+++ D.Util.Logger.Log4net/Log4netWriter.cs
@@ -0,0 +1,25 @@
+﻿using D.Util.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Util.Logger
+{
+    /// <summary>
+    /// 封装 log4net 到自定义的接口 ILogWriter
+    /// </summary>
+    public class Log4netWriter : ILogWriter
+    {
+        public Log4netWriter()
+        {
+
+        }
+
+        public void Write(ILogContext context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
