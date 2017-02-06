c1027e82fc37e5da30acfcc01d206d330f18cc51
Alger <AlgerDu@outlook.com>
2017-02-06 16:17:17 +0800

    添加新的项目，封装 cef 用于下载页面

---
+++ D.Spider.CefDownloader/CefDownloader.cs
@@ -0,0 +1,23 @@
+﻿using D.Spider.Core.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D.Spider.Core.Events;
+
+namespace D.Spider.Core
+{
+    public class CefDownloader : IDownloader
+    {
+        public void Handle(UrlWaitingEvent e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
