35cce2b31cea1bac9ba533f4dfdf7141aa620446
Alger <AlgerDu@outlook.com>
2017-02-15 20:49:09 +0800

    添加新的项目封装 cefsharp

---
+++ D.Spider.CefDownloader/CefBrowserMainForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace D.Spider
+{
+    /// <summary>
+    /// 封装的 cef 浏览器主窗口
+    /// </summary>
+    public partial class CefBrowserMainForm : Form
+    {
+        public CefBrowserMainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ D.Spider.CefDownloader/CefDownloader.cs
@@ -0,0 +1,31 @@
+﻿using D.Spider.Core.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using D.Spider.Core.Events;
+
+namespace D.Spider
+{
+    /// <summary>
+    /// 对 cefsharp 的封装用来爬取动态网页
+    /// </summary>
+    public class CefDownloader : IDownloader
+    {
+        /// <summary>
+        /// 处理 urlManager 发布的 UrlWaitingEvent
+        /// 
+        /// </summary>
+        /// <param name="e"></param>
+        public void Handle(UrlWaitingEvent e)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
