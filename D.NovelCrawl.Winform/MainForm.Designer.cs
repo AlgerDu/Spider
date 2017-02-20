eb9ceafad62d09b5e9eb9ffc443877735e628c5a
Alger <AlgerDu@outlook.com>
2017-02-20 20:54:07 +0800

    将cef展示在一个panl中

--- D.NovelCrawl.Winform/MainForm.Designer.cs
+++ D.NovelCrawl.Winform/MainForm.Designer.cs
@@ -28,19 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.P_Cef = new System.Windows.Forms.Panel();
             this.SuspendLayout();
+            // 
+            // P_Cef
+            // 
+            this.P_Cef.Location = new System.Drawing.Point(13, 13);
+            this.P_Cef.Name = "P_Cef";
+            this.P_Cef.Size = new System.Drawing.Size(995, 411);
+            this.P_Cef.TabIndex = 0;
             // 
             // MainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1020, 403);
+            this.ClientSize = new System.Drawing.Size(1020, 436);
+            this.Controls.Add(this.P_Cef);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
             this.Name = "MainForm";
-            this.Text = "MainForm";
+            this.Text = "小说爬虫";
+            this.Load += new System.EventHandler(this.MainForm_Load);
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel P_Cef;
     }
 }
--- D.NovelCrawl.Winform/MainForm.cs
+++ D.NovelCrawl.Winform/MainForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using D.Spider.Core.Interface;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,14 +8,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using D.Spider.Core.Events;
+using D.Util.Interface;
+using CefSharp.WinForms;
 
 namespace D.NovelCrawl.Winform
 {
+    /// <summary>
+    /// 程序主窗口，同时也承担承载 cef 浏览器的责任
+    /// </summary>
     public partial class MainForm : Form
+        , IDownloader
     {
         public MainForm()
         {
             InitializeComponent();
+        }
+
+        #region IDownloader 实现
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+
+        void IEventHandler<UrlWaitingEvent>.Handle(UrlWaitingEvent e)
+        {
+            throw new NotImplementedException();
+        }
+        #endregion
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            CefSharp.Cef.Initialize();
+
+            //实例化控件
+            ChromiumWebBrowser wb = new ChromiumWebBrowser("http://www.baidu.com");
+            //设置停靠方式
+            wb.Dock = DockStyle.Fill;
+
+            //加入到当前窗体中
+            P_Cef.Controls.Add(wb);
         }
     }
 }
