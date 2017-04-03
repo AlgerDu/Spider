7ffc7f50cf904660605d8e0c24a8d8abcc82ace8
Alger <AlgerDu@outlook.com>
2017-04-03 09:28:55 +0800

    添加新的接口

---
+++ D.Util/Interface/IMapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Util.Interface
+{
+    /// <summary>
+    /// 暂时只有最基本的映射
+    /// </summary>
+    public interface IMapper
+    {
+    }
+}
