480d6548e9e98bdb647b102c9b912abe54a911ae
Alger <AlgerDu@outlook.com>
2017-05-01 23:12:46 +0800

    添加配置相关的接口

---
+++ D.Util/Interface/IConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Util.Interface
+{
+    /// <summary>
+    /// 配置文件读取和保存
+    /// </summary>
+    public interface IConfig
+    {
+        /// <summary>
+        /// 获取某个模块的配置
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name"></param>
+        /// <returns></returns>
+        T GetItem<T>(string name = null) where T : IConfigItem, new();
+
+        /// <summary>
+        /// 将持有的配置项保存到文件
+        /// </summary>
+        void Save();
+    }
+}
---
+++ D.Util/Interface/IConfigItem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Util.Interface
+{
+    /// <summary>
+    /// 配置文件中不同模块的配置项
+    /// 请不要使用 new 的方式产生一个 item，请使用 IConfig.GetItem 方法
+    /// </summary>
+    public interface IConfigItem
+    {
+        string Path { get; }
+    }
+}
