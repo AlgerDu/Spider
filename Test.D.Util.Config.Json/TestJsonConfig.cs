ef7be8b5497e12d0c1b0458378d70b204e79fb75
Alger <AlgerDu@outlook.com>
2017-05-02 19:21:50 +0800

    添加测试项目

---
+++ Test.D.Util.Config.Json/TestJsonConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Test.D.Util.Config.Json
+{
+    [TestClass]
+    public class TestJsonConfig
+    {
+        /// <summary>
+        /// 测试自动生成配置文件
+        /// </summary>
+        [TestMethod]
+        public void TestAutoCreateConfigFile()
+        {
+            const string filePath = "TestAutoCreateConfigFile.json";
+
+            if (File.Exists(filePath))
+            {
+                File.Delete(filePath);
+            }
+        }
+    }
+}
