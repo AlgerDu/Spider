c552ce973a83707927e238785ab4f5731ffc477c
Alger <AlgerDu@outlook.com>
2017-05-06 22:11:30 +0800

    添加配置文件

---
+++ D.Util.Logger.Log4net/Log4netConfig.cs
@@ -0,0 +1,38 @@
+﻿using D.Util.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Util.Logger
+{
+    /// <summary>
+    /// log4net 配置
+    /// </summary>
+    internal class Log4netConfig : IConfigItem
+    {
+        public string Path
+        {
+            get
+            {
+                return "logging.log4net";
+            }
+        }
+
+        /// <summary>
+        /// 日志记录级别
+        /// </summary>
+        public string LogLevel { get; set; }
+
+        /// <summary>
+        /// 日志文件保存路径
+        /// </summary>
+        public string OutFilePath { get; set; }
+
+        /// <summary>
+        /// 输出日志文件最大的大小
+        /// </summary>
+        public string MaxFileSize { get; set; }
+    }
+}
