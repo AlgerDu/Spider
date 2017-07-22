76a1d9a4c279d71d27b0dbb2d4099c50f8aea022
Alger <AlgerDu@outlook.com>
2017-07-22 16:45:01 +0800

    添加新的接口

---
+++ D.Spider.Core/Interface/Plugin/IPluginEventHandler.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Spider.Core.Interface.Plugin
+{
+    /// <summary>
+    /// 插件事件处理器
+    /// </summary>
+    public interface IPluginEventHandler<TE>
+        where TE : IPluginEvent
+    {
+        /// <summary>
+        /// 处理事件，返回值暂定为 bool 标志成功与失败
+        /// TODO：可能需要扩充
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        bool Handle(TE e);
+    }
+}
