d177f55fe662a60b12e905566f010a39bf97180d
Alger <AlgerDu@outlook.com>
2017-07-22 12:13:46 +0800

    相关文件迁移，旧文件保留，待整理

---
+++ D.Spider.SpiderScriptEngine/Interface/ISsKeywordHandler.cs
@@ -0,0 +1,37 @@
+﻿using D.Spider.Core.SpiderScriptEngine;
+using NSoup.Nodes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Spider.Core.Interface
+{
+    /// <summary>
+    /// ISpiderscriptEngine 关键字处理器 策略模式
+    /// </summary>
+    internal interface ISsKeywordHandler
+    {
+        /// <summary>
+        /// handler 处理的关键字
+        /// </summary>
+        SsKeywordTypes Type { get; }
+
+        /// <summary>
+        /// 解析 code 字符串
+        /// 不包含此类型关键字返回 null
+        /// </summary>
+        /// <param name="line"></param>
+        /// <returns></returns>
+        SsCodeLine Analysis(string line);
+
+        /// <summary>
+        /// 执行代码
+        /// </summary>
+        /// <param name="context"></param>
+        /// <param name="ele"></param>
+        /// <param name="scope"></param>
+        void Execute(SsContext context, SsCodeLine line, Element ele, SsScope scope);
+    }
+}
