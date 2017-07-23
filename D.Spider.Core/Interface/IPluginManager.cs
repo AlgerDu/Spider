155d9b7be161b3309e1609ebff37b64c77caf64d
Alger <AlgerDu@outlook.com>
2017-07-23 13:51:05 +0800

    接口定义

---
+++ D.Spider.Core/Interface/IPluginManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Spider.Core.Interface
+{
+    /// <summary>
+    /// 插件管理器
+    /// </summary>
+    public interface IPluginManager
+    {
+
+    }
+}
