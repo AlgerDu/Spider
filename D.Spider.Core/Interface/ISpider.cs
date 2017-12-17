8438daef2141035a2135a60c1bec8168661fb8f9
Alger <AlgerDu@outlook.com>
2017-12-17 20:46:02 +0800

    删除不需要的项目

--- D.Spider.Core/Interface/ISpider.cs
+++ D.Spider.Core/Interface/ISpider.cs
@@ -15,36 +15,17 @@ namespace D.Spider.Core.Interface
     /// 内部维护一个依赖注入的容器
     /// </summary>
     public interface ISpider
-        : IEventHandler<UrlCrawledEvent>
     {
-        /// <summary>
-        /// IOc Unity 的容器
-        /// </summary>
-        IUnityContainer UnityContainer { get; }
-
-        /// <summary>
-        /// Url 管理器
-        /// </summary>
-        IUrlManager UrlManager { get; }
-
-        /// <summary>
-        /// 设置 spider 内部使用的 unity 框架的配置文件路径
-        /// TODO 暂时只能想到这样实现
-        /// </summary>
-        /// <param name="path"></param>
-        /// <returns></returns>
-        ISpider UnityConfigerPath(string path);
-
-        /// <summary>
-        /// 初始化整个 spider
-        /// </summary>
-        /// <returns></returns>
-        ISpider Initialization();
-
         /// <summary>
         /// 启动 Spider 开始爬取网页内容
         /// </summary>
         /// <returns>用于链式编程</returns>
         ISpider Run();
+
+        /// <summary>
+        /// 停止
+        /// </summary>
+        /// <returns></returns>
+        ISpider Stop();
     }
 }
---
+++ D.Spider.Core/Interface/ISpiderBuilder.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Spider.Core.Interface
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public interface ISpiderBuilder
+    {
+        /// <summary>
+        /// 使用 Startup 类
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        ISpiderBuilder UseStartup<T>() where T : ISpiderBuilder, new();
+
+        /// <summary>
+        /// 创建一个 Spider
+        /// </summary>
+        /// <returns></returns>
+        ISpider Build();
+    }
+}
