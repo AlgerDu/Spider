b2a26412e0dd427e369f5dfc90cac42b5e447fcf
Alger <AlgerDu@outlook.com>
2017-12-19 22:58:58 +0800

    example 分开文件

---
+++ D.Spider.Console.Example/MiniPlugin.cs
@@ -0,0 +1,46 @@
+﻿using D.Spider.Core.Interface.Plugin;
+using D.Util.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D.Spider.Example
+{
+    /// <summary>
+    /// 最简单插件
+    /// </summary>
+    class MiniPlugin : IPlugin
+    {
+        ILogger _logger;
+
+        IPluginSymbol _symbol;
+        PluginState _pluginState;
+
+        public IPluginSymbol Symbol => _symbol;
+
+        public PluginState State => _pluginState;
+
+        public MiniPlugin(
+            ILoggerFactory loggerFactory
+            )
+        {
+            _logger = loggerFactory.CreateLogger<MiniPlugin>();
+        }
+
+        public IPlugin Run()
+        {
+            _logger.LogInformation($"{Symbol} run");
+
+            return this;
+        }
+
+        public IPlugin Stop()
+        {
+            _logger.LogInformation($"{Symbol} stop");
+
+            return this;
+        }
+    }
+}
--- D.Spider.Console.Example/Program.cs
+++ D.Spider.Console.Example/Program.cs
@@ -1,39 +1,8 @@
-﻿using Autofac;
-using D.Spider.Core;
-using D.Spider.Core.Interface;
-using D.Util.Interface;
-using D.Utils.AutofacExt;
+﻿using D.Spider.Core;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace D.Spider.Example
 {
-    /// <summary>
-    /// IStartup 样例
-    /// </summary>
-    class Startup : IStartup
-    {
-        public void CollectConfig(IConfigCollector configCollector)
-        {
-            Console.WriteLine("Startup 收集配置");
-        }
-
-        public void ConfigService(ContainerBuilder builder)
-        {
-            Console.WriteLine("Startup 配置服务（依赖注入）");
-
-            builder.AddConsoleLogWriter();
-        }
-
-        public void ManualCollectPlugin(IPluginCollecter pluginCollecter)
-        {
-            Console.WriteLine("Startup 手动添加插件");
-        }
-    }
-
     class Program
     {
         static void Main(string[] args)
